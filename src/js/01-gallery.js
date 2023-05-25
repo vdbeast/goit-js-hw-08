23681157a3b1f48743bfc60f5acf16acfa66011d
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-05-25 22:03:07 +0300

    added simplelightbox task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,42 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function initGallery(items) {
+    let galleryElements = items.map(item => {
+        const itemEL = document.createElement('li');
+        itemEL.classList.add('gallery__item');
+    
+        let linkEl = document.createElement('a');
+        linkEl.classList.add('gallery__link');
+        linkEl.href = item.original;
+
+        let imgEl = document.createElement('img');
+        imgEl.classList.add('gallery__image');
+        imgEl.src = item.preview;
+        imgEl.alt = item.description;
+        imgEl.dataset.source = item.original;
+
+
+        itemEL.appendChild(linkEl);
+        linkEl.appendChild(imgEl)
+
+        return linkEl;
+    });
+ 
+    galleryEl.append(...galleryElements);
+}
+
+initGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
