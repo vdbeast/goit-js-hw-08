b3216e8ed7323ca0a70a44e00406f15d526a668d
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-05-29 13:01:47 +0300

    added task-2 and task-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+function saveCurrentTime() {
+  player
+    .getCurrentTime()
+    .then((currentTime) => {
+      localStorage.setItem(STORAGE_KEY, currentTime);
+    })
+    .catch((error) => {
+      console.error('Error saving current time:', error);
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const emailInput = formEl.querySelector('input[name="email"]');
+const messageInput = formEl.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
+
+const updatedStorage = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}, 500);
+
+const completedFields = () => {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
+};
+
+formEl.addEventListener('input', () => {
+  updatedStorage();
+});
+
+window.addEventListener('load', () => {
+  completedFields();
+});
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  localStorage.removeItem(STORAGE_KEY);
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log('Submitted:', {
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+});
